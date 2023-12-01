e3a1688310e353a38c95a4855d912d6478c9d500
Andres Moral Carballo <andremoralcarballo@gmail.com>
2023-12-01 17:45:15 -0400

    Add: controlador de empresas y sus rutas, terminadas

--- app/Http/Controllers/EmpresasController.php
+++ app/Http/Controllers/EmpresasController.php
@@ -5,45 +5,162 @@ namespace App\Http\Controllers;
 use App\Models\Empresas;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\Rule;
+
 class EmpresasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    //AGREGAR
     public function store(Request $request)
     {
-        //
+
+        //VALIDACION DE CAMPOS
+        $validator = Validator::make($request->all(), [
+            'nombreEmpresa' => ['required', 'string', 'max:50', 'unique:empresas'],
+            'rubroEmpresa' => ['required', 'string', 'max:50'],
+            'celularEmpresa' => ['required', 'string', 'max:15', 'min:8', 'regex:/^[0-9()+-]*$/'],
+            'direccionEmpresa' => ['required', 'string', 'max:50'],
+            'correoEmpresa' => ['nullable', 'email']
+        ], [
+            'celularEmpresa.min' => 'El numero de celular no tiene los digitos suficientes.',
+            'nombreEmpresa.unique' => 'El nombre de la empresa ya existe.'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_NOT_FOUND);
+        }
+
+        $empresa = new Empresas;
+        $empresa->nombreEmpresa = $request->nombreEmpresa;
+        $empresa->rubroEmpresa = $request->rubroEmpresa;
+        $empresa->celularEmpresa = $request->celularEmpresa;
+        $empresa->direccionEmpresa = $request->direccionEmpresa;
+        $empresa->correoEmpresa = $request->correoEmpresa;
+        $empresa->estadoEmpresa = 1; // Establecer el estado a 1
+        $empresa->save();
+
+        return response()->json(['message' => 'Se agregó a ' . $request->nombreEmpresa]);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Empresas $empresas)
+
+
+    //MOSTRAR
+    public function show($id)
     {
-        //
+
+        try {
+            $empresa = Empresas::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'No se encontró a la empresa para mostrarla.'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $empresa;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Empresas $empresas)
+
+
+    //ACTUALIZAR
+    public function update(Request $request, $id)
     {
-        //
+
+        try {
+            $empresa = Empresas::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'No se encontró a la empresa para actualizarla.'], Response::HTTP_NOT_FOUND);
+        }
+
+
+        //VALIDACION DE CAMPOS
+        $validator = Validator::make($request->all(), [
+            'nombreEmpresa' => ['required', 'string', 'max:50', Rule::unique('empresas')->ignore($id, 'id_empresa')],
+            'rubroEmpresa' => ['required', 'string', 'max:50'],
+            'celularEmpresa' => ['required', 'string', 'max:50', 'regex:/^[0-9()+-]*$/'],
+            'direccionEmpresa' => ['required', 'string', 'max:50'],
+            'correoEmpresa' => ['nullable', 'email']
+        ], [
+            'nombreEmpresa.unique' => 'El nombre de la empresa ya existe.'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_NOT_FOUND);
+        }
+
+        $empresa->nombreEmpresa = $request->nombreEmpresa;
+        $empresa->rubroEmpresa = $request->rubroEmpresa;
+        $empresa->celularEmpresa = $request->celularEmpresa;
+        $empresa->direccionEmpresa = $request->direccionEmpresa;
+        $empresa->correoEmpresa = $request->correoEmpresa;
+        $empresa->save();
+
+        return response()->json(['message' => 'La empresa ' . $request->nombreEmpresa .  ' ha sido actualizada exitosamente.']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Empresas $empresas)
+
+
+    //BORRAR
+    public function destroy($id)
     {
-        //
+        try {
+            $empresa = Empresas::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'No se encontró a la empresa para eliminarla.'], Response::HTTP_NOT_FOUND);
+        }
+
+        $empresa->delete();
+        return response()->json(['message' => 'La empresa' . $empresa->nombreEmpresa . ' ha sido borrada exitosamente.']);
+    }
+
+    // MOSTRAR EMPRESAS DESCENDENTE
+    public function index()
+    {
+        return Empresas::orderBy('updated_at', 'desc')->get();
+    }
+
+    //MOSTRAR EMPRESAS ASCENDENTE
+    public function ascendente()
+    {
+        return Empresas::orderBy('updated_at', 'asc')->get();
+    }
+
+    // MOSTRAR EMPRESAS EN UN RANGO
+    public function rangoDeFechas(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'fechaInicio' => 'required|date',
+            'fechaFin' => 'required|date|after_or_equal:fechaInicio',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+
+        $fechaInicio = $request->fechaInicio;
+        $fechaFin = $request->fechaFin;
+
+        $personasEnRango = Empresas::whereBetween('updated_at', [$fechaInicio, $fechaFin])->get();
+
+        return response()->json(['empresas' => $personasEnRango]);
+    }
+
+    // BUSCADOR DE EMPRESAS
+    public function buscadorEmpresas(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nombreEmpresa' => ['required', 'string']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+
+        $nombreEmpresa = $request->nombreEmpresa;
+
+        // Buscar empresas que coincidan con el nombre proporcionado
+        $empresas = Empresas::where('nombreEmpresa', 'LIKE', '%' . $nombreEmpresa . '%')->get();
+
+        return response()->json(['empresas' => $empresas]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmpresasController;
 use App\Http\Controllers\PersonasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,16 @@ Route::get('mostrarPersona/{id}', [PersonasController::class, 'show']);
 Route::put('actualizarPersona/{id}', [PersonasController::class, 'update']);
 Route::delete('borrarPersona/{id}', [PersonasController::class, 'destroy']);
 Route::get('personasDescendente', [PersonasController::class, 'index']);
-Route::get('personasAscendente', [PersonasController::class, 'personasAscendente']);
-Route::post('personasRango', [PersonasController::class, 'personasPorRangoDeFechas']);
+Route::get('personasAscendente', [PersonasController::class, 'ascendente']);
+Route::post('personasRango', [PersonasController::class, 'rangoDeFechas']);
 Route::post('buscadorPersonas', [PersonasController::class, 'buscadorPersonas']);
+
+// EMPRESAS
+Route::post('agregarEmpresa', [EmpresasController::class, 'store']);
+Route::get('mostrarEmpresa/{id}', [EmpresasController::class, 'show']);
+Route::put('actualizarEmpresa/{id}', [EmpresasController::class, 'update']);
+Route::delete('borrarEmpresa/{id}', [EmpresasController::class, 'destroy']);
+Route::get('empresasDescendente', [EmpresasController::class, 'index']);
+Route::get('empresasAscendente', [EmpresasController::class, 'ascendente']);
+Route::post('empresasRango', [EmpresasController::class, 'rangoDeFechas']);
+Route::post('buscadorEmpresas', [EmpresasController::class, 'buscadorEmpresas']);
